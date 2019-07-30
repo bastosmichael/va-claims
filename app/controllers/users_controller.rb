37cb28bfe88483ad12e9a7885483c281977e693f
Michael Bastos <bastosmichael@gmail.com>
2019-07-30 08:30:37 -0500

    creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
     @name = @session.id_token_attributes['name']
     name_parts = @name.split(' ')
     @user = TestUser.where('lower(first_name) = ? AND  lower(last_name) = ?', name_parts.first.downcase, name_parts.last.downcase).first
+    @user = TestUser.create(first_name: name_parts.first.downcase, last_name: name_parts.last.downcase) unless @user
   end
 end
